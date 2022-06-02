8f2f268cb39c0f423d3c3101056f4ba6ae6425ec
Nathan <38576090+TheTrueCoder@users.noreply.github.com>
2022-06-02 21:05:03 +1200

    Make it delete old vendorised bundle

--- build.py
+++ build.py
@@ -1,9 +1,14 @@
 import vendorize.cli as vendorize
 from shutil import make_archive, copytree, rmtree
+from os.path import join
 
 ADDON_NAME: str = "cool-addon"
 CODE_DIR: str = "src"
+DEP_FOLDER_NAME: str = "_vendor"
+DEP_PATH = join(CODE_DIR, DEP_FOLDER_NAME)
 
+# Delete old package bundle to avoid stale packages in the build.
+rmtree(DEP_PATH)
 # Download packages into the _vendor folder to be used in the addon.
 # Configure the downloaded packages in vendorize.toml.
 vendorize.main()
