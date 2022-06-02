736d0b3f36a8b6da7635bc63011b95c7ecbfddf3
Nathan <38576090+TheTrueCoder@users.noreply.github.com>
2022-06-02 16:43:18 +1200

    Fix build script

--- build.py
+++ build.py
@@ -1,12 +1,16 @@
 import vendorize.cli as vendorize
-from shutil import make_archive
+from shutil import make_archive, copytree, rmtree
 
-ADDON_NAME: str = "addon"
+ADDON_NAME: str = "cool-addon"
 CODE_DIR: str = "src"
 
 # Download packages into the _vendor folder to be used in the addon.
 # Configure the downloaded packages in vendorize.toml.
 vendorize.main()
 
+# Make temporary folder copy with the project name so that
+# the addon is identifiable when installed.
+copytree(CODE_DIR, ADDON_NAME)
 # Zip addon for easy distribution
-make_archive(ADDON_NAME, "zip", CODE_DIR)
+make_archive(ADDON_NAME, "zip", ".", ADDON_NAME)
+rmtree(ADDON_NAME)
