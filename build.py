51d6bbfbe7393f7699eeead748e35cbe5d5deefb
Nathan <38576090+TheTrueCoder@users.noreply.github.com>
2022-06-05 21:20:01 +1200

    Add comments to build script

--- build.py
+++ build.py
@@ -2,17 +2,20 @@ import vendorize.cli as vendorize
 from shutil import make_archive, copytree, rmtree
 from os.path import join
 
-ADDON_NAME: str = "cool-addon"
+ADDON_NAME: str = "cue-blender"
 CODE_DIR: str = "src"
 DEP_FOLDER_NAME: str = "_vendor"
+
 DEP_PATH = join(CODE_DIR, DEP_FOLDER_NAME)
 
+# MAKE VENDORISED PACKAGE BUNDLE
 # Delete old package bundle to avoid stale packages in the build.
 rmtree(DEP_PATH)
 # Download packages into the _vendor folder to be used in the addon.
 # Configure the downloaded packages in vendorize.toml.
 vendorize.main()
 
+# MAKE ZIP PACKAGE
 # Make temporary folder copy with the project name so that
 # the addon is identifiable when installed.
 copytree(CODE_DIR, ADDON_NAME)
